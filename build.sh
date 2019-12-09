d5d75ff73009dbfe3a1ebd9aa8e3b45e40065496
MrSwaggerr <48083120+MrSwaggerr@users.noreply.github.com>
2019-12-09 12:32:03 +0100

    updated

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ docker run --rm --privileged \
     -v ~/.docker:/root/.docker \
     homeassistant/amd64-builder \
     --all \
-    -r https://github.com/korylprince/hassio-caddy.git \
+    -r https://github.com/mrswaggerr/hassio-caddy.git \
     -b master \
     -t caddy
